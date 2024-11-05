65b2deee2e3acd3514f6a58155437e89830701f6
그램 <darkzhon@hanmail.net>
2024-11-05 16:41:58 +0900

    페이지 네이션 적용하기

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,19 +5,19 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
  
 export default async function Page(props:{searchParams?:Promise<{query?:string, page?:string}>}) {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-
   // <Search> => 클라이언트 컴포넌트, useSearchParams() hook을 사용.
   //             사용자의 입력을 url에 전달하기 때문에 page에 있는 props를 사용하기 애매함.
   //             또한 클라이언트에서 바로 params를 읽어서 속도가 빠름. (서버를 경유하지 않음)
 
   // <Table>  => 서버 컴포넌트, hook을 사용할수없다. 
   //             데이터베이스의 데이터를 가져오기위해 page.tsx에서 searchParams prop을 전달.
-  
+  const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -31,7 +31,7 @@ export default async function Page(props:{searchParams?:Promise<{query?:string, 
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -36,27 +36,33 @@ export const generateYAxis = (revenue: Revenue[]) => {
 };
 
 export const generatePagination = (currentPage: number, totalPages: number) => {
-  // If the total number of pages is 7 or less,
-  // display all pages without any ellipsis.
+  // 만약 totalPages가 7개 이하 이면 
+  // 모든 페이지를 보여준다.
   if (totalPages <= 7) {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
 
-  // If the current page is among the first 3 pages,
-  // show the first 3, an ellipsis, and the last 2 pages.
+  // 만약 totalPages가 7개 이상이고, currentPage가 3 이하이면 
+  // 처음 3개는 보여주고 나머지는 ... 마지막과 바로전은 보여준다.
+  // 예시 totalPage : 8 currentPage : 3
+  // 1 2 3 ... 7 8 
   if (currentPage <= 3) {
     return [1, 2, 3, '...', totalPages - 1, totalPages];
   }
 
   // If the current page is among the last 3 pages,
   // show the first 2, an ellipsis, and the last 3 pages.
+  // 만약 currentPage가 마지막 3개 중에 하나라면
+  // 처음 2개는 보여주고 나머지는 ... 마지막 3개를 보여준다.
+  //예시 totalpage : 8 currentPage : 6
+  // 1 2 ... 6 7 8
   if (currentPage >= totalPages - 2) {
     return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages];
   }
 
-  // If the current page is somewhere in the middle,
-  // show the first page, an ellipsis, the current page and its neighbors,
-  // another ellipsis, and the last page.
+  // 모든 조건이 아닌 중간부분이면 맨처음, 마지막 1개 현재 페이지와 바로 앞 뒤 번호를 보여준다.
+  //예시 totalpage : 8 currentPage : 5
+  // 1 ... 4 5 6 ... 8
   return [
     1,
     '...',
